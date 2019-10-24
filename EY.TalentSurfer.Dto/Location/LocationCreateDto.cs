0af791a378ccd9ba59ef3200ee1a60a75282cb47
Joaquin Garcia <joaquin.garcia@globant.com>
2019-10-24 09:47:04 -0300

    fix US1 Oppt Locations validations for create and update

--- EY.TalentSurfer.Dto/Location/LocationCreateDto.cs
+++ EY.TalentSurfer.Dto/Location/LocationCreateDto.cs
@@ -1,12 +1,22 @@
 ﻿using EY.TalentSurfer.Support.Services.Contracts;
+using System.ComponentModel.DataAnnotations;
 
 namespace EY.TalentSurfer.Dto
 {
     public class LocationCreateDto : ICreateDto
     {
+        [Required]
+        [Range(0, int.MaxValue)]
         public int SortOrder { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
         public string Description { get; set; }
+
+        [Required]
         public bool ArchivingFlag { get; set; }
+
+        [MaxLength(1000)]
         public string Comments { get; set; }
     }
 }
