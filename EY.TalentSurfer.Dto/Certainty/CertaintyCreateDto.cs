4913417dfc15e56cb5b15a7b1ae7a4df79ca59ed
Ana Rodriguez <ana.rodriguez@globant.com>
2019-11-27 10:52:00 -0500

    Adding validation to CertaintyUpdateDto and CertaintyCreateDto (#68)
    * [Bug 32 and 34] US2 - Opportunity Certainty - Missing validations in POST-PUT methods
    
    * Adding validation to CertaintyUpdateDto
    
    * Update OpportunityConfiguration class

--- EY.TalentSurfer.Dto/Certainty/CertaintyCreateDto.cs
+++ EY.TalentSurfer.Dto/Certainty/CertaintyCreateDto.cs
@@ -1,12 +1,19 @@
 ﻿using EY.TalentSurfer.Support.Services.Contracts;
+using System.ComponentModel.DataAnnotations;
 
 namespace EY.TalentSurfer.Dto
 {
     public class CertaintyCreateDto : ICreateDto
     {
+        [Required]
+        [StringLength(1000)]
         public string Description { get; set; }
+        [Required]
+        [StringLength(1000)]
         public string Value { get; set; }
         public bool ArchivingFlag { get; set; }
+        [Required]
+        [StringLength(1000)]
         public string Comments { get; set; }
     }
 }
